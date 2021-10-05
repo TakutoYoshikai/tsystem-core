829d315e91105f15d917d40b6ef80fa77b44fd8c
lucky <takuto.yoshikai@gmail.com>
2021-10-06 01:23:22 +0900

    homebrew

--- bin/install-homebrew.sh
+++ bin/install-homebrew.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if [ "`command -v brew`" = "" ]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
